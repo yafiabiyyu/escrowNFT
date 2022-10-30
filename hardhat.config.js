8e47bdf6e6c4173d2640224b22187ee23783963e
Abiyyu Yafi <yafiabiyyu@protonmail.com>
2022-10-31 01:30:40 +0700

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: "0.8.16",
+    networks: {
+        alfajores: {
+            url: "https://alfajores-forno.celo-testnet.org/",
+            chainId: 44787,
+            accounts:{
+                mnemonic: process.env.MNEMONIC,
+                path: "m/44'/60'/0'/0"
+            }
+        }
+    },
+    gasReporter:{
+        token: 'CELO',
+        currency: 'USD',
+        gasPrice: 21,
+        enabled: true
+    }
 };
