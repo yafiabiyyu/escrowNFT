ee5f7e9bb2593555e1c24317de204a85617d97bb
yafiabiyyu <yafiabiyyu@protonmail.com>
2022-12-01 15:28:22 +0000

    update info

--- frontend/src/pages/info.tsx
+++ frontend/src/pages/info.tsx
@@ -13,127 +13,294 @@ import {
 } from "@mui/material";
 import { useOrderData } from "../utils/escrow";
 import dayjs, { Dayjs } from "dayjs";
+import { ethers } from "ethers";
+import { useEthers } from "@usedapp/core";
+import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 interface EscrowOrder {
     escrowId: string;
     tokenId: string;
     paymentAmount: string;
-    deadline: Dayjs;
+    deadline: string;
+    deadlineObject: Dayjs | null;
     nftAddress: string;
     buyerAddress: string;
     sellerAddress: string;
     status: string;
 }
 
-interface FormState {
-    search: boolean;
-    form: boolean;
-    loading: boolean;
-}
-
-interface StateSnackbar {
-    open: boolean;
-    type: AlertColor;
-    message: string;
-}
-
 const Info: FC<any> = (): ReactElement => {
+    const [formSearch, setFormSearch] = React.useState(true);
+    const [formData, setFormData] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+    const timer = React.useRef<number>();
 
-    const [ formStatus, setFormStatus ] = React.useState<FormState>({
-        search: true,
-        form: false,
-        loading: false
-    });
-
-    const [ alert, setAlert] = React.useState<StateSnackbar>({
-        open: false,
-        type: "success",
-        message:""
-    });
-
-    const [ order, setOrder ] = React.useState<EscrowOrder>({
-        escrowId:"",
-        tokenId:"",
-        paymentAmount:"",
-        deadline: dayjs(),
+    const [order, setOrder] = React.useState<EscrowOrder>({
+        escrowId: "",
+        tokenId: "",
+        paymentAmount: "",
+        deadline: "",
+        deadlineObject: null,
         nftAddress: "",
-        buyerAddress:"",
-        sellerAddress:"",
-        status:""
+        buyerAddress: "",
+        sellerAddress: "",
+        status: ""
     })
-    const orderData = useOrderData(order.escrowId);
 
-    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setOrder({
-            ...order, escrowId: event.target.value
-        });
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setOrder({ ...order, escrowId: event.target.value });
     }
+    const escrowOrder = useOrderData(order.escrowId);
 
     const handleButtonSearch = () => {
-        if(order.escrowId === "") {
-            setAlert({
-                ...alert,
-                open:true,
-                type:"warning",
-                message:"Escrow ID cannot be empty"
-            });
-        }else {
-            setAlert({
-                ...alert,
-                open:false
-            });
-            if(order.nftAddress === "0x0000000000000000000000000000000000000000") {
-                console.log(order)
-            }else {
-                console.log(order);
-            }
-        }
-    }
-
-    React.useEffect(() => {
-        if(orderData !== undefined) {
+        if (escrowOrder === "Error" || escrowOrder === undefined) {
+            console.log("Error")
+        } else if (escrowOrder === "Null") {
+            console.log("Not found");
+        } else {
             let status = "";
-            if(orderData?.[6] === 0) {
+            if (escrowOrder?.[6] === 0) {
                 status = "Pending";
-            } else if(orderData?.[6] === 1) {
+            } else if (escrowOrder?.[6] === 1) {
                 status = "Accepted";
-            } else if(orderData?.[6] === 2) {
+            } else if (escrowOrder?.[6] === 2) {
                 status = "Rejected";
-            }else {
+            } else if (escrowOrder?.[6] === 3) {
                 status = "Canceled";
             }
             setOrder({
                 ...order,
+                tokenId: escrowOrder?.[0].toString(),
+                paymentAmount: escrowOrder?.[1].toString(),
+                deadline: escrowOrder?.[2].toString(),
+                deadlineObject: dayjs.unix(escrowOrder?.[2].toNumber()),
+                nftAddress: escrowOrder?.[3],
+                buyerAddress: escrowOrder?.[4],
+                sellerAddress: escrowOrder?.[5],
                 status: status
             });
+            console.log(order);
+            setFormSearch(false);
+            setFormData(true);
+            setLoading(true);
+            timer.current = window.setTimeout(() => {
+                setLoading(false);
+            }, 10000)
         }
-    }, [orderData]);
+    }
+
+    React.useEffect(() => {
+        return () => {
+            clearTimeout(timer.current);
+        }
+    }, [])
 
     return (
-        <Box sx={{mt: 5, display:"flex", flexDirection:"column", alignItems:"center"}}>
-            <Snackbar open={alert.open} autoHideDuration={3000}>
-                <Alert severity={alert.type}>{alert.message}</Alert>
-            </Snackbar>
-            <Typography component="h1" variant="h5">
-                Escrow Info
-            </Typography>
-            { formStatus.search && !formStatus.form && (
+        <Box sx={{
+            mt: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+        }}>
+            <Typography component="h1" variant="h5">Escrow Info</Typography>
+            {formSearch && !formData && (
                 <React.Fragment>
-                    <Box sx={{ mt: 3}}>
+                    <Box sx={{ mt: 3 }}>
                         <Grid container spacing={2}>
                             <Grid item xs={12}>
-                                <TextField onChange={handleInput} name="escrowId" fullWidth id="escrowId" label="Escrow Id" size="medium"/>
+                                <TextField
+                                    label="Escrow ID"
+                                    name="escrowId"
+                                    id="escrowId"
+                                    size="medium"
+                                    fullWidth
+                                    onChange={handleOnChange} />
                             </Grid>
                             <Grid item xs={12}>
-                                <Button onClick={handleButtonSearch} size="large" fullWidth variant="contained">
-                                    Check Escrow Order
-                                </Button>
+                                <Button
+                                    variant="contained"
+                                    size="medium"
+                                    fullWidth
+                                    onClick={handleButtonSearch}>Check Escrow</Button>
                             </Grid>
                         </Grid>
+                    </Box>
+                </React.Fragment>
+            )}
+            {!formSearch && formData && (
+                <React.Fragment>
+                    <Box sx={{ mt: 3 }}>
+                        {loading ? (
+                            <Grid container spacing={3}>
+                                <Grid item xs={4}>
+                                    <Skeleton
+                                        variant="rounded"
+                                        width={140}
+                                        height={23}
+                                    />
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Skeleton
+                                        variant="rounded"
+                                        width={140}
+                                        height={23}
+                                    />
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Skeleton
+                                        variant="rounded"
+                                        width={140}
+                                        height={23}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Skeleton
+                                        variant="rounded"
+                                        width={524}
+                                        height={23}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Skeleton
+                                        variant="rounded"
+                                        width={524}
+                                        height={23}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Skeleton
+                                        variant="rounded"
+                                        width={524}
+                                        height={23}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Skeleton
+                                        variant="rounded"
+                                        width={524}
+                                        height={23}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Skeleton
+                                        variant="rounded"
+                                        width={524}
+                                        height={23}
+                                    />
+                                </Grid>
+                            </Grid>
+                        ) : (
+                            <Grid container spacing={2}>
+                                <Grid item xs={6}>
+                                    <TextField
+                                        label="Escrow ID"
+                                        name="escrowId"
+                                        id="escrowId"
+                                        size="medium"
+                                        value={order.escrowId}
+                                        fullWidth
+                                        InputProps={{ readOnly: true }} />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                        <DateTimePicker
+                                        label="Deadline"
+                                        readOnly
+                                        value={order.deadlineObject}
+                                        onChange={(test) => {
+                                            setOrder({...order,deadlineObject:test})
+                                        }}
+                                        renderInput={(params) => (
+                                            <TextField {...params}
+                                            fullWidth/>
+                                        )} />
+                                    </LocalizationProvider>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        label="Seller Address"
+                                        name="sellerAddress"
+                                        id="sellerAddress"
+                                        size="medium"
+                                        value={order.sellerAddress}
+                                        fullWidth
+                                        InputProps={{ readOnly: true }} />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        label="Buyer Address"
+                                        name="buyerAddress"
+                                        id="buyerAddress"
+                                        size="medium"
+                                        value={order.buyerAddress}
+                                        fullWidth
+                                        InputProps={{ readOnly: true }} />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        label="NFT Address"
+                                        name="nftAddress"
+                                        id="nftAddress"
+                                        size="medium"
+                                        value={order.nftAddress}
+                                        fullWidth
+                                        InputProps={{ readOnly: true }} />
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <TextField
+                                        label="Token ID"
+                                        name="tokenId"
+                                        id="tokenId"
+                                        size="medium"
+                                        value={order.tokenId}
+                                        fullWidth
+                                        InputProps={{ readOnly: true }} />
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <TextField
+                                        label="Payment Amount"
+                                        name="paymentAmount"
+                                        id="paymentAmount"
+                                        size="medium"
+                                        value={ethers.utils.formatEther(order.paymentAmount)}
+                                        fullWidth
+                                        InputProps={{ readOnly: true }} />
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <TextField
+                                        label="Status"
+                                        name="status"
+                                        id="status"
+                                        size="medium"
+                                        value={order.status}
+                                        fullWidth
+                                        InputProps={{ readOnly: true }} />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button size="medium" variant="contained" fullWidth>
+                                        Cancle Escrow
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button href="/info" size="medium" variant="contained" color="secondary" fullWidth>
+                                        Back
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        )}
+                        {formData && !loading && (
+                            <Grid container justifyContent="flex-end" sx={{ mt: 2 }}>
+                                <Grid item>
+                                    <Link href="/info" variant="body2">Already have an escrow id? Check status</Link>
+                                </Grid>
+                            </Grid>
+                        )}
                     </Box>
                 </React.Fragment>
             )}
         </Box>
     )
 }
+
 export default Info;
